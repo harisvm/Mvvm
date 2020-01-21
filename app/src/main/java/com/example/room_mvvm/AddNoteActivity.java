b23169bc639fee818cf6b22091c3e781dbb7e7bf
harry <harinarayanan.vm@macomsolutions.com>
2020-01-21 17:08:55 +0530

    save note activity - on progress

--- app/src/main/java/com/example/room_mvvm/AddNoteActivity.java
+++ app/src/main/java/com/example/room_mvvm/AddNoteActivity.java
@@ -1,18 +1,31 @@
 package com.example.room_mvvm;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddNoteActivity extends AppCompatActivity {
     NumberPicker numberPicker;
+    private EditText title, des;
+    public static String EXTRA_TITLE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
 
+
+        title = findViewById(R.id.edt_title);
+        des = findViewById(R.id.edt_description);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        getSupportActionBar().setTitle("Add Note");
         numberPicker = findViewById(R.id.numberPicker);
         numberPicker.setMinValue(1);
         numberPicker.setMaxValue(10);
@@ -22,5 +35,36 @@ public class AddNoteActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.saveicon):
+
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void saveNote() {
+        String editTitle = title.getText().toString();
+        String editDes = des.getText().toString();
+        int prio = numberPicker.getValue();
+
+        if (editTitle.trim().isEmpty() || editDes.trim().isEmpty()) {
+            Toast.makeText(this, "Please add title or description", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
