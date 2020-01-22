27fe18e59726e77bf3d675a8c7b2234c06a11cd9
Harrys <harrys@Harryss-MacBook-Pro.local>
2020-01-22 06:37:37 +0530

    live data code on main activty

--- app/src/main/java/com/example/room_mvvm/AddNoteActivity.java
+++ app/src/main/java/com/example/room_mvvm/AddNoteActivity.java
@@ -1,5 +1,6 @@
 package com.example.room_mvvm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,59 +13,65 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddNoteActivity extends AppCompatActivity {
-    NumberPicker numberPicker;
-    private EditText title, des;
-    public static String EXTRA_TITLE;
+	NumberPicker numberPicker;
+	private EditText title, des;
+	public static final String EXTRA_TITLE = "com.example.room_mvvm.EXTRA_TITLE";
+	public static final String EXTRA_DESCRIPTION = "com.example.room_mvvm.EXTRA_DESCRIPTION";
+	public static final String EXTRA_PRIORITY = "com.example.room_mvvm.EXTRA_PRIORITY";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_note);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_note);
 
 
-        title = findViewById(R.id.edt_title);
-        des = findViewById(R.id.edt_description);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
-        getSupportActionBar().setTitle("Add Note");
-        numberPicker = findViewById(R.id.numberPicker);
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(10);
-        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+		title = findViewById(R.id.edt_title);
+		des = findViewById(R.id.edt_description);
+		getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+		getSupportActionBar().setTitle("Add Note");
+		numberPicker = findViewById(R.id.numberPicker);
+		numberPicker.setMinValue(1);
+		numberPicker.setMaxValue(10);
 
-            }
-        });
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.add_note_menu, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.add_note_menu, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case (R.id.saveicon):
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		switch (item.getItemId()) {
+			case (R.id.saveicon):
 
-                saveNote();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+				saveNote();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
 
-        }
-    }
+		}
+	}
 
-    private void saveNote() {
-        String editTitle = title.getText().toString();
-        String editDes = des.getText().toString();
-        int prio = numberPicker.getValue();
+	private void saveNote() {
+		String editTitle = title.getText().toString();
+		String editDes = des.getText().toString();
+		int prio = numberPicker.getValue();
 
-        if (editTitle.trim().isEmpty() || editDes.trim().isEmpty()) {
-            Toast.makeText(this, "Please add title or description", Toast.LENGTH_SHORT).show();
-            return;
-        }
-    }
+		if (editTitle.trim().isEmpty() || editDes.trim().isEmpty()) {
+			Toast.makeText(this, "Please add title or description", Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		Intent data = new Intent();
+		data.putExtra(EXTRA_TITLE, editTitle);
+		data.putExtra(EXTRA_DESCRIPTION, editDes);
+		data.putExtra(EXTRA_PRIORITY, prio);
+		setResult(RESULT_OK, data);
+		finish();
+
+
+	}
 }
